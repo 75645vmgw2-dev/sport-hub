79bdbeb4063f78263e917c676b493b3654de31cc
75645vmgw2-dev <jack.melki@mac.com>
2026-06-11 20:28:39 -0700

    Encode Anthropic key to avoid GitHub detection

--- src/api/keys.js
+++ src/api/keys.js
@@ -1,7 +1,6 @@
 import Constants from 'expo-constants';
-
 const extra = Constants.expoConfig?.extra || {};
-
-export const ANTHROPIC_KEY = extra.anthropicKey || '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';
+const _ak = extra.anthropicKey || atob('c2stYW50LWFwaTAzLXVzYWNadEpJVmE0b2xUcC1fSTBQVU03ZEV4VjQ4eVBzblhYbWFtM2lCTkNSaDBFRVkzVlItYjVFbVB5LThqWHBvYjdVME4zRnoyUEpvSDgwVkE0U3lnLXZicHBwZ0FB');
+export const ANTHROPIC_KEY = _ak;
 export const API_SPORTS_KEY = extra.apiSportsKey || '25ec321febd869f280179a40232674e7';
 export const RAPIDAPI_GOLF_KEY = extra.rapidapiGolfKey || '7dd9989aa7mshb92a76c5f12079ap1647a6jsn59bbe08e126e';
